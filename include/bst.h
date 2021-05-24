47f1c1f10b170166bb02999497d5836b8b083088
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-05-24 20:48:15 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,13 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include<string>
+
+struct Node{
+  std::string data;
+  int count;
+  Node* left;
+  Node* right;
+}
 
 #endif  // INCLUDE_BST_H_
