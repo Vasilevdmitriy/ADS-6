64e8cfcabfbc864196cb7c67940c50881f19111b
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-05-29 11:18:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,10 +8,8 @@
 BST<std::string> makeTree(const char* filename) {
      std::ifstream file(filename);
      BST<std::string> tree;
-     std::string temp1;
-     std::string word = "";
-     int i;
-     int ban;
+     std::string word;
+     char ch;
      int c =32;
     if (!file) {
         std::cout << "File error!" << std::endl;
@@ -19,25 +17,18 @@ BST<std::string> makeTree(const char* filename) {
     }
     
     while(!file.eof()){
-        file >> temp1;
-            i = 0;
-           while(i < temp1.size()){
-               ban = 0;
-               while((temp1[i] >= 'a') && (temp1[i] <= 'z') || (temp1[i] >= 'A') && (temp1[i] <= 'Z')) {
-                  if ((temp1[i] >= 'A') && (temp1[i] <= 'Z'))
-                    temp1[i] += c;
-                    word +=temp1[i];
-                ban=1;
-                i++;
-              }
-              
-              if(ban != 0){
-                  tree.add(word);
-                    word.clear();
-              }
-              i++;
-           }
-           
+        ch=file.get();
+        if(((ch >= 'a')&&(ch <= 'z'))||((ch >= 'A')&&(ch <= 'Z'))){
+            if((ch >= 'A')&&(ch <= 'Z'))
+                    ch +=c;
+                word +=ch;
+               
+        } else if(word != ""){
+                 std::cout<<word<<std::endl;
+                tree.add(word);
+                word.clear();
+
+        }
     }
     
     file.close();
