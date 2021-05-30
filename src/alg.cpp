3e4dced39f301176d15ec8cdf4a8f8d28b351780
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-05-30 07:14:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,7 @@ BST<std::string> makeTree(const char* filename) {
   std::ifstream file(filename);
   if (!file) {
     std::cout << "File error!" << std::endl;
-    return Tree;
+    return tree;
   }
   while (!file.eof()) {
     char ch = file.get();
